fc9d0f79dd5c3cc5ae389b97e79769a13bb985f2
Yudai NAKATA <yudai.nkt@gmail.com>
2022-05-31 22:14:21 +0900

    build: use a dedicated Vite plugin

---
+++ src/manifest.ts
@@ -0,0 +1,22 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+
+export const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Refined Zenn",
+  description: "Zennの機能やUIの一部を変更するChrome拡張機能です．",
+  version: "0.1.0",
+  content_scripts: [
+    {
+      matches: ["https://zenn.dev/*"],
+      js: ["src/content-scripts/sticky-header.ts"],
+    },
+    {
+      matches: ["https://zenn.dev/*/articles/*"],
+      js: ["src/content-scripts/footnote-tooltip.ts"],
+    },
+  ],
+  options_ui: {
+    page: "src/options-ui/index.html",
+  },
+  permissions: ["storage"],
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-import { defineConfig, type Plugin } from "vite";
+import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-
-// In order to not delete manifest.json and assets built by ESbuild,
-// Vite needs to set output directory to `/dist/vite`.
-// However, manifest.json sets serving root to `/dist`, so the script element
-// needs to be modified accordingly.
-const replaceScriptURI: Plugin = {
-  name: "replace-script-uri",
-  transformIndexHtml: {
-    transform: (html) => html.replace(/src="(.*)"/, 'src="/vite$1"'),
-    enforce: "post",
-  },
-};
+import { crx } from "@crxjs/vite-plugin";
+import { manifest } from "./src/manifest";
 
 export default defineConfig({
-  root: "src",
-  build: {
-    rollupOptions: {
-      input: { "options-ui": "./src/options-ui/index.html" },
-    },
-    outDir: "../dist/vite",
-    emptyOutDir: true,
-  },
-  plugins: [preact(), replaceScriptURI],
+  plugins: [preact(), crx({ manifest })],
 });
